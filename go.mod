4a9efda0ec28d6f9d1e27ffae3e9884032b628c4
EXisTAnZ <ozim_ssspam@mail.ru>
2023-12-29 19:05:00 +0300

    feat: add sqlite driver and create tables

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-tnved
 
 go 1.21.2
+
+require github.com/mattn/go-sqlite3 v1.14.19
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	fmt.Println("Start!")
+	db, _ := sql.Open("sqlite3", "./output/tnved.db")
+	createTables(db)
+	fmt.Println("Finished!")
+	db.Close()
+
+}
+
+func createTables(db *sql.DB) {
+	create, _ := db.Prepare("CREATE TABLE IF NOT EXISTS tnved1 (RAZDEL CHARACTER(20), NAIM CHARACTER(4000), PRIM CHARACTER(4000), DATA DATE, DATA1 DATE)")
+	create.Exec()
 }
