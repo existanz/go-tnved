ce3dcbca832bab473b8499166939e2b622282c82
EXisTAnZ <ozim_ssspam@mail.ru>
2023-12-30 21:55:33 +0300

    feat: initialize tables in database

--- main.go
+++ main.go
@@ -7,16 +7,97 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type TCol struct {
+	name   string
+	dbType string
+}
+
+type Table struct {
+	name   string
+	desc   string
+	fields []TCol
+}
+
+// tnved tables
+var tables []Table = []Table{
+	{
+		name: "tnved1",
+		desc: "Разделы",
+		fields: []TCol{
+			{name: "RAZDEL", dbType: "CHARACTER(2)"},
+			{name: "NAIM", dbType: "CHARACTER(4000)"},
+			{name: "PRIM", dbType: "CHARACTER(4000)"},
+			{name: "DATA", dbType: "DATE"},
+			{name: "DATA1", dbType: "DATE"},
+		},
+	},
+	{
+		name: "tnved2",
+		desc: "Группы",
+		fields: []TCol{
+			{name: "RAZDEL", dbType: "CHARACTER(2)"},
+			{name: "GRUPPA", dbType: "CHARACTER(2)"},
+			{name: "NAIM", dbType: "CHARACTER(4000)"},
+			{name: "PRIM", dbType: "CHARACTER(4000)"},
+			{name: "DATA", dbType: "DATE"},
+			{name: "DATA1", dbType: "DATE"},
+		},
+	},
+	{
+		name: "tnved3",
+		desc: "Товарные позиции",
+		fields: []TCol{
+			{name: "GRUPPA", dbType: "CHARACTER(2)"},
+			{name: "TOV_POZ", dbType: "CHARACTER(2)"},
+			{name: "NAIM", dbType: "CHARACTER(4000)"},
+			{name: "DATA", dbType: "DATE"},
+			{name: "DATA1", dbType: "DATE"},
+		},
+	},
+	{
+		name: "tnved4",
+		desc: "Товарные подпозиции",
+		fields: []TCol{
+			{name: "GRUPPA", dbType: "CHARACTER(2)"},
+			{name: "TOV_POZ", dbType: "CHARACTER(2)"},
+			{name: "SUB_POZ", dbType: "CHARACTER(6)"},
+			{name: "KR_NAIM", dbType: "CHARACTER(200)"},
+			{name: "DATA", dbType: "DATE"},
+			{name: "DATA1", dbType: "DATE"},
+		},
+	},
+}
+
 func main() {
 	fmt.Println("Start!")
 	db, _ := sql.Open("sqlite3", "./output/tnved.db")
-	createTables(db)
+	defer db.Close()
+	initTables(db, tables)
 	fmt.Println("Finished!")
-	db.Close()
 
 }
 
-func createTables(db *sql.DB) {
-	create, _ := db.Prepare("CREATE TABLE IF NOT EXISTS tnved1 (RAZDEL CHARACTER(20), NAIM CHARACTER(4000), PRIM CHARACTER(4000), DATA DATE, DATA1 DATE)")
-	create.Exec()
+func initTables(db *sql.DB, tables []Table) {
+	for _, table := range tables {
+		createQuery := "CREATE TABLE IF NOT EXISTS " + table.name + " ("
+		for _, field := range table.fields {
+			createQuery += field.name + " " + field.dbType + ", "
+		}
+		createQuery = createQuery[:len(createQuery)-2] + ")"
+		deleteQuery := "DELETE FROM " + table.name
+		fmt.Println("Create table " + table.name + " - " + table.desc)
+		create, _ := db.Prepare(createQuery)
+		create.Exec()
+		create.Close()
+		delete, _ := db.Prepare(deleteQuery)
+		delete.Exec()
+		delete.Close()
+		fmt.Println("Table " + table.name + " successfuly initialized")
+	}
+}
+
+func unpack(s []string, vars ...*string) {
+	for i := range vars {
+		*vars[i] = s[i]
+	}
 }
